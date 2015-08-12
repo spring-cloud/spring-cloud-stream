5bafdc7d37c1d9c7ad961e58e00bef357ca8a4fb
Eric Bottard <eric.bottard+github@gmail.com>
2015-08-12 17:34:30 +0200

    XD-3381: Provide test support for modules

--- spring-cloud-stream-test-support/src/main/java/org/springframework/cloud/stream/test/binder/TestSupportBinder.java
+++ spring-cloud-stream-test-support/src/main/java/org/springframework/cloud/stream/test/binder/TestSupportBinder.java
@@ -15,18 +15,39 @@
 
 package org.springframework.cloud.stream.test.binder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
-import org.springframework.cloud.stream.binder.Binder;
+import org.springframework.cloud.stream.binder.AbstractBinderPropertiesAccessor;
+import org.springframework.cloud.stream.binder.Binding;
+import org.springframework.cloud.stream.binder.MessageChannelBinderSupport;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.endpoint.AbstractEndpoint;
+import org.springframework.integration.endpoint.EventDrivenConsumer;
+import org.springframework.integration.handler.BridgeHandler;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 /**
- * Created by ericbottard on 12/08/15.
+ * A minimal binder that<ul>
+ *     <li>does nothing about binding consumers, leaving the channel as-is, so that a test author can interact with it directly,</li>
+ *     <li>registers a queue channel on the producer side, so that it is easy to assert what is received.</li>
+ * </ul>
+ *
+ * @author Eric Bottard
+ * @see org.springframework.cloud.stream.test.matcher.MessageChannelMatcher
  */
-public class TestSupportBinder implements Binder<MessageChannel> {
+public class TestSupportBinder extends MessageChannelBinderSupport {
+
+	public static Map<MessageChannel, QueueChannel> channelToBindings =
+			new HashMap<>();
+
+
 	@Override
 	public void bindConsumer(String name, MessageChannel inboundBindTarget, Properties properties) {
-
+		// Do nothing. A module author can grab hold of the input channel
+		// and interact with it directly
 	}
 
 	@Override
@@ -34,33 +55,31 @@ public class TestSupportBinder implements Binder<MessageChannel> {
 
 	}
 
+	/**
+	 * Bridges a QueueChannel to the module output channel, so that it can
+	 * be easily queried by {@link org.springframework.cloud.stream.test.matcher.MessageChannelMatcher}.
+	 */
 	@Override
 	public void bindProducer(String name, MessageChannel outboundBindTarget, Properties properties) {
+		BridgeHandler bridge = new BridgeHandler();
+		QueueChannel queue = new QueueChannel();
+		bridge.setOutputChannel(queue);
+		AbstractEndpoint ae = new EventDrivenConsumer((SubscribableChannel) outboundBindTarget, bridge);
+		ae.setBeanName("outbound." + name);
+		Binding binding = Binding.forConsumer(name, ae, outboundBindTarget, new NullPropertiesAccessor(properties));
+		addBinding(binding);
+		channelToBindings.put(outboundBindTarget, queue);
+		binding.start();
+	}
 
+	@Override
+	public void unbindProducer(String name, MessageChannel channel) {
+		super.unbindProducer(name, channel);
+		channelToBindings.remove(channel);
 	}
 
 	@Override
 	public void bindPubSubProducer(String name, MessageChannel outboundBindTarget, Properties properties) {
-
-	}
-
-	@Override
-	public void unbindConsumers(String name) {
-
-	}
-
-	@Override
-	public void unbindProducers(String name) {
-
-	}
-
-	@Override
-	public void unbindConsumer(String name, MessageChannel inboundBindTarget) {
-
-	}
-
-	@Override
-	public void unbindProducer(String name, MessageChannel outboundBindTarget) {
 
 	}
 
@@ -74,18 +93,11 @@ public class TestSupportBinder implements Binder<MessageChannel> {
 
 	}
 
-	@Override
-	public MessageChannel bindDynamicProducer(String name, Properties properties) {
-		return null;
-	}
 
-	@Override
-	public MessageChannel bindDynamicPubSubProducer(String name, Properties properties) {
-		return null;
-	}
+	private static class NullPropertiesAccessor extends AbstractBinderPropertiesAccessor {
 
-	@Override
-	public boolean isCapable(Capability capability) {
-		return false;
+		public NullPropertiesAccessor(Properties properties) {
+			super(properties);
+		}
 	}
 }
--- spring-cloud-stream-test-support/src/main/java/org/springframework/cloud/stream/test/matcher/MessageChannelMatcher.java
+++ spring-cloud-stream-test-support/src/main/java/org/springframework/cloud/stream/test/matcher/MessageChannelMatcher.java
@@ -15,8 +15,121 @@
 
 package org.springframework.cloud.stream.test.matcher;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.SelfDescribing;
+
+import org.springframework.cloud.stream.test.binder.TestSupportBinder;
+import org.springframework.integration.util.Function;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+
 /**
- * Created by ericbottard on 12/08/15.
+ * A Hamcrest Matcher meant to be used in conjunction with {@link TestSupportBinder}.
+ *
+ * <p>Expected usage is of the form (with appropriate static imports):
+ * <pre>
+ * public class TransformProcessorApplicationTests {
+ *
+ *    {@literal @}Autowired
+ *    {@literal @}ModuleChannels(TransformProcessor.class)
+ *    private Processor processor;
+ *
+ *    {@literal @}Test
+ *    public void testUsingExpression() {
+ *    processor.input().send(new GenericMessage{@literal <}Object>("hello"));
+ *    assertThat(processor.output(), receivesPayloadThat(equalTo("hellofoo")).within(10));
+ *    }
+ *
+ * }</pre>
+ * </p>
+ *
+ * @author Eric Bottard
  */
-public class MessageChannelMatcher {
+public class MessageChannelMatcher<T> extends BaseMatcher<MessageChannel> {
+
+	private final Matcher<T> delegate;
+
+	private final long timeout;
+
+	private Extractor<Message<?>, T> extractor;
+
+	private Map<MessageChannel, T> actuallyReceived = new HashMap<>();
+
+	public MessageChannelMatcher(Matcher<T> delegate, long timeout, Extractor<Message<?>, T> extractor) {
+		this.delegate = delegate;
+		this.timeout = timeout;
+		this.extractor = extractor;
+	}
+
+
+	@Override
+	public boolean matches(Object item) {
+		MessageChannel channel = (MessageChannel) item;
+		Message<?> received = TestSupportBinder.channelToBindings.get(channel).receive(timeout);
+		if (received == null) { // timeout
+			return false;
+		}
+		T unwrapped = extractor.apply(received);
+		actuallyReceived.put(channel, unwrapped);
+		return delegate.matches(unwrapped);
+	}
+
+	@Override
+	public void describeMismatch(Object item, Description description) {
+		T value = actuallyReceived.get(item);
+		if (value != null) {
+			description.appendText("received: ").appendValue(value);
+		} else {
+			description.appendText("timed out after " + timeout + "ms.");
+		}
+	}
+
+	public MessageChannelMatcher within(long timeout) {
+		return new MessageChannelMatcher(this.delegate, timeout, extractor);
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("Channel to receive ").appendDescriptionOf(extractor).appendDescriptionOf(delegate);
+	}
+
+	public static <P> MessageChannelMatcher receivesMessageThat(Matcher<Message<P>> messageMatcher) {
+		return new MessageChannelMatcher(messageMatcher, -1, new Extractor<Message<P>, Message<P>>("a message that ") {
+			@Override
+			public Message<P> apply(Message<P> m) {
+				return m;
+			}
+		});
+	}
+
+	public static <P> MessageChannelMatcher receivesPayloadThat(Matcher<P> payloadMatcher) {
+		return new MessageChannelMatcher(payloadMatcher, -1, new Extractor<Message<P>, P>("a message whose payload ") {
+			@Override
+			public P apply(Message<P> m) {
+				return m.getPayload();
+			}
+		});
+	}
+
+	private static abstract class Extractor<R, T> implements Function<R, T>, SelfDescribing {
+
+		private final String behaviorDescription;
+
+		protected Extractor(String behaviorDescription) {
+			this.behaviorDescription = behaviorDescription;
+		}
+
+		@Override
+		public void describeTo(Description description) {
+			description.appendText(behaviorDescription);
+		}
+	}
+
+
+
 }
