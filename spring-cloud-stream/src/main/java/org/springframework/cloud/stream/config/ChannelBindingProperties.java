08f898cb22631f04f634713f30e07972728da062
Marius Bogoevici <mbogoevici@pivotal.io>
2015-08-20 17:27:41 -0400

    Use application.name in default binding name
    Enable autoconfiguration so property placeholders are handled

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/config/ChannelBindingProperties.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/config/ChannelBindingProperties.java
@@ -20,7 +20,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -34,6 +36,9 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 public class ChannelBindingProperties {
 
 	public static final String PATH = "path";
+
+	@Value("${spring.application.name:}")
+	private String springApplicationName;
 
 	private Properties consumerProperties = new Properties();
 
@@ -81,7 +86,8 @@ public class ChannelBindingProperties {
 			}
 		}
 		// the default path of the binding is the channel name itself
-		return channelName;
+		return (StringUtils.hasText(springApplicationName)? springApplicationName + "." : "")
+				+  channelName;
 	}
 
 	public String getTapChannelName(String channelName) {
