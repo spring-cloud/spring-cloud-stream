af2ea6de1c102667bd18c3b3bbdc4de6e7755714
Marius Bogoevici <mbogoevici@pivotal.io>
2015-08-19 21:36:45 -0400

    Add Channel Interceptor Support tests

---
+++ spring-cloud-stream/src/test/java/org/springframework/cloud/stream/interceptor/ChannelsInterceptedTests.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.interceptor;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.stream.annotation.EnableModule;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.ModuleChannels;
+import org.springframework.cloud.stream.annotation.Sink;
+import org.springframework.cloud.stream.binder.Binder;
+import org.springframework.cloud.stream.utils.MockBinderConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.config.GlobalChannelInterceptor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Marius Bogoevici
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(ChannelsInterceptedTests.Foo.class)
+public class ChannelsInterceptedTests {
+
+	public static final Message<?> TEST_MESSAGE = MessageBuilder.withPayload("bar").build();
+
+	@Autowired
+	ChannelInterceptor channelInterceptor;
+
+	@Autowired
+	@ModuleChannels(ChannelsInterceptedTests.Foo.class)
+	public Sink fooSink;
+
+	@Test
+	public void testChannelsIntercepted() {
+		fooSink.input().send(TEST_MESSAGE);
+		Mockito.verify(channelInterceptor).preSend(TEST_MESSAGE, fooSink.input());
+		Mockito.verifyNoMoreInteractions(channelInterceptor);
+	}
+
+
+	@SpringBootApplication
+	@EnableModule(Sink.class)
+	@Import(MockBinderConfiguration.class)
+	public static class Foo {
+
+		@ServiceActivator(inputChannel = Sink.INPUT)
+		public void fooSink(Message<?> message) {
+		}
+
+		@GlobalChannelInterceptor @Bean
+		public ChannelInterceptor globalChannelInterceptor() {
+			return mock(ChannelInterceptor.class);
+		}
+
+	}
+}
