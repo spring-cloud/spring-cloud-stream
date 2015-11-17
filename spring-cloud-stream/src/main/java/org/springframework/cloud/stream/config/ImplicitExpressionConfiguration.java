036cfaf0e858973ea2d9dc5e341b58bd619e60e1
Eric Bottard <eric.bottard+github@gmail.com>
2015-11-17 15:47:09 +0100

    Introduce synthetic configuration properties via @ImplicitExpression
    This fixes #177

---
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/config/ImplicitExpressionConfiguration.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.config;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.configurationmetadata.ConfigurationMetadataProperty;
+import org.springframework.boot.configurationmetadata.ConfigurationMetadataRepositoryJsonBuilder;
+import org.springframework.cloud.stream.annotationprocessor.ImplicitExpressionProcessor;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.ClassUtils;
+
+/**
+ * A configuration that installs a low level {@link org.springframework.core.env.PropertySource}
+ * to support {@link org.springframework.cloud.stream.annotationprocessor.ImplicitExpression} properties.
+ *
+ * @author Eric Bottard
+ * @author Florent Biville
+ */
+@Configuration
+public class ImplicitExpressionConfiguration implements EnvironmentAware, PriorityOrdered {
+
+
+	private Map<String, ConfigurationMetadataProperty> allProperties;
+
+	public ImplicitExpressionConfiguration() throws IOException {
+		ConfigurationMetadataRepositoryJsonBuilder builder = ConfigurationMetadataRepositoryJsonBuilder.create();
+		ResourcePatternResolver moduleResourceLoader = new PathMatchingResourcePatternResolver();
+		for (Resource r : moduleResourceLoader.getResources("classpath*:" + ImplicitExpressionProcessor.METADATA_FILE)) {
+			builder.withJsonResource(r.getInputStream());
+		}
+		allProperties = builder.build().getAllProperties();
+	}
+
+	@Override
+	public void setEnvironment(final Environment environment) {
+		if (environment instanceof ConfigurableEnvironment) {
+			ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) environment;
+
+			Map<String, Object> map = new HashMap<>();
+			MapPropertySource fakeValues = new MapPropertySource("Implicit expressions", map);
+			for (Map.Entry<String, ConfigurationMetadataProperty> entry : allProperties.entrySet()) {
+				String raw = environment.getProperty(entry.getKey());
+				if (raw != null) {
+					String type = entry.getValue().getType();
+					map.put(entry.getKey() + "-expression", literalExpression(raw, type));
+				}
+			}
+			configurableEnvironment.getPropertySources().addLast(fakeValues);
+		}
+	}
+
+	/**
+	 * Return a literal SpEL expression with the given value, using proper literal notation according to the wanted
+	 * type.
+	 */
+	private String literalExpression(Object value, String type) {
+		Class<?> clazz = ClassUtils.resolveClassName(type, Thread.currentThread().getContextClassLoader());
+		if (CharSequence.class.isAssignableFrom(clazz)) {
+			return String.format("'%s'", value);
+		}
+		else if (Number.class.isAssignableFrom(clazz)) {
+			return value.toString();
+		}
+		else {
+			return value.toString();
+		}
+	}
+
+	private String implicitPropertyName(String explicitPropertyName) {
+		return explicitPropertyName.substring(0, explicitPropertyName.length() - "-expression".length());
+	}
+
+	@Override
+	public int getOrder() {
+		return PriorityOrdered.HIGHEST_PRECEDENCE;
+	}
+}
---
+++ spring-cloud-stream/src/test/java/org/springframework/cloud/stream/config/ImplicitExpressionConfigurationTests.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.config;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.stream.annotationprocessor.ImplicitExpression;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.expression.Expression;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Tests for ImplicitExpressionConfiguration.
+ *
+ * @author Eric Bottard
+ * @author Florent Biville
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {
+		SpelExpressionConverterConfiguration.class,
+		ImplicitExpressionConfiguration.class,
+		ImplicitExpressionConfigurationTests.Config.class
+})
+public abstract class ImplicitExpressionConfigurationTests {
+
+	@Autowired
+	protected FooProperties fooProperties;
+
+
+	@IntegrationTest("foobar.name = foo")
+	public static class ImplicitExpressionSetsValueTests extends ImplicitExpressionConfigurationTests {
+
+		@Test
+		public void test() {
+			Assert.assertThat(fooProperties.getNameExpression().getExpressionString(), is("'foo'"));
+		}
+	}
+
+	@IntegrationTest("foobar.other = 42")
+	public static class ImplicitExpressionForNonStringTypeTests extends ImplicitExpressionConfigurationTests {
+
+		@Test
+		public void test() {
+			Assert.assertThat(fooProperties.getOtherExpression().getExpressionString(), is("42"));
+		}
+	}
+
+	@IntegrationTest("foobar.name = foo")
+	public static class ValueNotSetTests extends ImplicitExpressionConfigurationTests {
+
+		@Test
+		public void test() {
+			Assert.assertThat(fooProperties.getOtherExpression(), is(nullValue()));
+		}
+	}
+
+	@IntegrationTest({"foobar.name = foo", "foobar.nameExpression = foobar"})
+	public static class ExplicitExpressionOverridesTests extends ImplicitExpressionConfigurationTests {
+
+		@Test
+		public void test() {
+			Assert.assertThat(fooProperties.getNameExpression().getExpressionString(), is("foobar"));
+		}
+	}
+
+
+	@Configuration
+	@EnableIntegration
+	@EnableConfigurationProperties(FooProperties.class)
+	public static class Config {
+
+
+	}
+
+
+	@ConfigurationProperties(prefix = "foobar")
+	public static class FooProperties {
+
+		@ImplicitExpression(value = "the name")
+		private Expression nameExpression;
+
+		@ImplicitExpression(value = "an other thing", type = Integer.class)
+		private Expression otherExpression;
+
+		public Expression getOtherExpression() {
+			return otherExpression;
+		}
+
+		public void setOtherExpression(Expression otherExpression) {
+			System.out.println("Setting1 " + otherExpression);
+			this.otherExpression = otherExpression;
+		}
+
+		public Expression getNameExpression() {
+			return nameExpression;
+		}
+
+		public void setNameExpression(Expression nameExpression) {
+			System.out.println("Setting2 " + nameExpression);
+			this.nameExpression = nameExpression;
+		}
+	}
+
+}
