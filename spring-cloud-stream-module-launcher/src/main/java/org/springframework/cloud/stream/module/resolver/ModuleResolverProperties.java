373dd12fbb817bd112da347bbc3c3336bf9f59ff
Mark Pollack <mpollack@pivotal.io>
2015-12-22 10:45:43 -0500

    Change remoteRepositories default to libs-snapshot post release

--- spring-cloud-stream-module-launcher/src/main/java/org/springframework/cloud/stream/module/resolver/ModuleResolverProperties.java
+++ spring-cloud-stream-module-launcher/src/main/java/org/springframework/cloud/stream/module/resolver/ModuleResolverProperties.java
@@ -37,7 +37,7 @@ public class ModuleResolverProperties {
 	/**
 	 * Location of comma separated remote maven repositories from which modules will be downloaded, if not available locally.
 	 */
-	private String[] remoteRepositories = new String[] {"https://repo.spring.io/libs-milestone"};
+	private String[] remoteRepositories = new String[] {"https://repo.spring.io/libs-snapshot"};
 
 	/**
 	 * Whether the resolver should operate in offline mode.
