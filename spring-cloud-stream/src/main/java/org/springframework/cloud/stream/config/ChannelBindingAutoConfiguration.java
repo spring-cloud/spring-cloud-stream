70b410af3aaf1569f60c02dfeb5f8cd036d9e554
Marius Bogoevici <mbogoevici@pivotal.io>
2016-01-13 13:28:05 -0500

    Update copyrights and address comments

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/config/ChannelBindingAutoConfiguration.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/config/ChannelBindingAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import org.springframework.messaging.MessageChannel;
  * general Spring Integration infrastructure.
  *
  * @author Dave Syer
+ * @author Marius Bogoevici
  */
 @Configuration
 @ConditionalOnBean(ChannelBindingService.class)
@@ -45,7 +46,7 @@ public class ChannelBindingAutoConfiguration {
 	private DefaultPollerProperties poller;
 
 	@Autowired(required = false)
-	List<Bindable> adapters;
+	private List<Bindable> adapters;
 
 	@Bean(name = PollerMetadata.DEFAULT_POLLER)
 	@ConditionalOnMissingBean(PollerMetadata.class)
