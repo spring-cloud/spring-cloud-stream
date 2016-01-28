aa36eb467dfc53d343b39eef033436e1507bcc33
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2016-01-28 16:51:00 +0530

    Add durableSubscription as a binding property
     - This will be updated as a binder consumer property for the key `durableSubscription`
    
    This resolves #293

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/config/ChannelBindingServiceProperties.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/config/ChannelBindingServiceProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -111,6 +111,10 @@ public class ChannelBindingServiceProperties {
 			if (bindingProperties.getConcurrency() != null) {
 				channelConsumerProperties.setProperty(BinderPropertyKeys.CONCURRENCY,
 						Integer.toString(bindingProperties.getConcurrency()));
+			}
+			if (bindingProperties.getDurableSubscription() != null) {
+				channelConsumerProperties.setProperty(BinderPropertyKeys.DURABLE,
+						Boolean.toString(bindingProperties.getDurableSubscription()));
 			}
 			updateConsumerPartitionProperties(inputChannelName, channelConsumerProperties);
 		}
