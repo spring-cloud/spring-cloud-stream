f88a49eaa78a3fa4c970cfbba43e38918111f95e
Marius Bogoevici <mbogoevici@pivotal.io>
2016-02-04 14:04:41 -0500

    GH-312 Add default constructor for TupleBuilder
    Resolves #312
    
    The TupleBuilder in XD provided a zero arg constructor. This restores backward compatibility by adding it back.

--- spring-cloud-stream-tuple/src/main/java/org/springframework/cloud/stream/tuple/TupleBuilder.java
+++ spring-cloud-stream-tuple/src/main/java/org/springframework/cloud/stream/tuple/TupleBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 the original author or authors.
+ * Copyright 2013-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,6 +82,13 @@ public class TupleBuilder {
 	 */
 	public static TupleBuilder tuple() {
 		return new TupleBuilder(false);
+	}
+
+	/**
+	 * Creates a builder that will create immutable Tuples.
+	 */
+	public TupleBuilder() {
+		this(false);
 	}
 
 	private TupleBuilder(boolean mutable) {
