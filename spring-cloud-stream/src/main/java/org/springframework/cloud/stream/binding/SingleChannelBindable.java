e77bcc3e4c51a97703b262885ddb35718c073cfe
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2016-03-02 11:18:39 +0530

    Make SingleChannelBindable more generic
     - Use `MessageChannel` instead of `PublishSubscribableChannel`
     - Remove error channel references

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/SingleChannelBindable.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/SingleChannelBindable.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.messaging.MessageChannel;
 
 /**
  * A {@link Bindable} component that wraps a generic channel. Useful for binding channels outside the
@@ -33,16 +33,16 @@ public class SingleChannelBindable extends BindableAdapter {
 
 	private final String name;
 
-	private final PublishSubscribeChannel errorChannel;
+	private final MessageChannel messageChannel;
 
-	public SingleChannelBindable(String name, PublishSubscribeChannel errorChannel) {
+	public SingleChannelBindable(String name, MessageChannel messageChannel) {
 		this.name = name;
-		this.errorChannel = errorChannel;
+		this.messageChannel = messageChannel;
 	}
 
 	@Override
 	public void bindOutputs(ChannelBindingService adapter) {
-		adapter.bindProducer(errorChannel, name);
+		adapter.bindProducer(messageChannel, name);
 	}
 
 	@Override
