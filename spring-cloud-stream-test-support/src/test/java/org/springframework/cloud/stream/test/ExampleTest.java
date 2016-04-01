7893904e0147cb890e7e2a503618f8a6109601f6
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2016-04-01 17:37:27 +0530

    Add doc for TestBinder
    This resolves #451

--- spring-cloud-stream-test-support/src/test/java/org/springframework/cloud/stream/test/ExampleTest.java
+++ spring-cloud-stream-test-support/src/test/java/org/springframework/cloud/stream/test/ExampleTest.java
@@ -19,9 +19,6 @@ package org.springframework.cloud.stream.test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.IntegrationTest;
@@ -30,13 +27,16 @@ import org.springframework.cloud.stream.annotation.Bindings;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.binder.BinderFactory;
 import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.cloud.stream.test.binder.TestSupportBinder;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Integration test that validates that {@link org.springframework.cloud.stream.test.binder.TestSupportBinder} applies
@@ -55,12 +55,15 @@ public class ExampleTest {
 	@Autowired
 	private BinderFactory<MessageChannel> binderFactory;
 
+	@Autowired
+	private MessageCollector messageCollector;
+
 	@Test
 	@SuppressWarnings("unchecked")
 	public void testWiring() {
 		Message<String> message = new GenericMessage<>("hello");
 		processor.input().send(message);
-		Message<String> received = (Message<String>) ((TestSupportBinder) binderFactory.getBinder(null)).messageCollector().forChannel(processor.output()).poll();
+		Message<String> received = (Message<String>) messageCollector.forChannel(processor.output()).poll();
 		assertThat(received.getPayload(), equalTo("hello world"));
 	}
 
