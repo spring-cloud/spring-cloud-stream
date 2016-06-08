65f04c7a8bc4297e223e45f87402196e92064346
Marius Bogoevici <mbogoevici@pivotal.io>
2016-06-08 10:53:54 -0400

    Polishing for GH-570
    - Add back old property in deprecated form for backwards compatibility.

--- spring-cloud-stream-binders/spring-cloud-stream-binder-rabbit/src/main/java/org/springframework/cloud/stream/binder/rabbit/config/RabbitBinderConfigurationProperties.java
+++ spring-cloud-stream-binders/spring-cloud-stream-binder-rabbit/src/main/java/org/springframework/cloud/stream/binder/rabbit/config/RabbitBinderConfigurationProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,16 @@ class RabbitBinderConfigurationProperties {
 
 	public void setAdminAddresses(String[] adminAddresses) {
 		this.adminAddresses = adminAddresses;
+	}
+
+	/**
+	 * @param adminAddresses A comma-separated list of RabbitMQ management plugin URLs.
+	 * @deprecated in favor of {@link #setAdminAddresses(String[])}. Will be removed in a
+	 * future release.
+	 */
+	@Deprecated
+	public void setAdminAdresses(String[] adminAddresses) {
+		setAdminAddresses(adminAddresses);
 	}
 
 	public String[] getNodes() {
