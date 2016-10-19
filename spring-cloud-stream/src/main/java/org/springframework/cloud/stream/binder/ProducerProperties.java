e7e799b8d8e7704e8e8e4df02ba28e2819793976
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2016-10-19 17:59:44 +0530

    Support native serialization in MessageChannelBinder
    Resolves #691
    
    - If the broker supports native serialization, then the serialization of the
       outbound payload is not done by the binder's producer binding outbound message handler.
    - At the consumer binding's inbound message handler, the extraction of embedded headers
       is performed only for the byte[] payload (given the deserialization would have already
       occurred at the broker level)
    - Add correspoinding tests in Kafka binder implementation
    - Rely on the received message type to decide on using out of the box deserializer (only
       if byte[] message payload is received)

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/ProducerProperties.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/ProducerProperties.java
@@ -50,6 +50,8 @@ public class ProducerProperties {
 
 	private HeaderMode headerMode = HeaderMode.embeddedHeaders;
 
+	private boolean useNativeEncoding = false;
+
 	public Expression getPartitionKeyExpression() {
 		return partitionKeyExpression;
 	}
@@ -119,6 +121,14 @@ public class ProducerProperties {
 
 	public void setHeaderMode(HeaderMode headerMode) {
 		this.headerMode = headerMode;
+	}
+
+	public boolean isUseNativeEncoding() {
+		return this.useNativeEncoding;
+	}
+
+	public void setUseNativeEncoding(boolean useNativeEncoding) {
+		this.useNativeEncoding = useNativeEncoding;
 	}
 
 }
