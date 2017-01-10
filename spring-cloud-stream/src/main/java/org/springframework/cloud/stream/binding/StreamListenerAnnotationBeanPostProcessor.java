aa8c14dcd221ae1bf552dc282fe5c931e9ac53e3
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2017-01-10 10:39:45 +0530

    Fix duplicate StreamListener mapping in parameterized classes
     - Add test
     - For StreamListener methods, filter out bridge methods
    
    Resolves #683

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/StreamListenerAnnotationBeanPostProcessor.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/StreamListenerAnnotationBeanPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,7 +108,7 @@ public class StreamListenerAnnotationBeanPostProcessor
 			@Override
 			public void doWith(final Method method) throws IllegalArgumentException, IllegalAccessException {
 				StreamListener streamListener = AnnotationUtils.findAnnotation(method, StreamListener.class);
-				if (streamListener != null) {
+				if (streamListener != null && !method.isBridge()) {
 					Assert.isTrue(method.getAnnotation(Input.class) == null,
 							StreamListenerErrorMessages.INPUT_AT_STREAM_LISTENER);
 					String methodAnnotatedInboundName = streamListener.value();
