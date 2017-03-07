a67fe02fd711f8ef26e25c0d90e553cf1310c3b8
Marius Bogoevici <mbogoevici@pivotal.io>
2017-03-07 14:32:07 -0500

    Handle `@StreamListener` properly as meta-annotation
    Fix #841
    
    * Use `AnnotatedElementUtils`
    * Formatting for the additional test

--- spring-cloud-stream-integration-tests/src/test/java/org/springframework/cloud/stream/config/StreamListenerAsMetaAnnotationTests.java
+++ spring-cloud-stream-integration-tests/src/test/java/org/springframework/cloud/stream/config/StreamListenerAsMetaAnnotationTests.java
@@ -1,6 +1,20 @@
-package org.springframework.cloud.stream.config;
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static org.assertj.core.api.Assertions.assertThat;
+package org.springframework.cloud.stream.config;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -12,6 +26,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.junit.Test;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -23,7 +38,11 @@ import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * @author David Turanski
+ */
 public class StreamListenerAsMetaAnnotationTests {
 	
 	@Test
