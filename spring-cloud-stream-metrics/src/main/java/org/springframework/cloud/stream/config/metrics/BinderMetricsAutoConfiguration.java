33b7e63ff15e1127ca64bbcb64e085a7c4010cd2
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2017-03-08 15:05:58 -0500

    Initial metrics implementation

---
+++ spring-cloud-stream-metrics/src/main/java/org/springframework/cloud/stream/config/metrics/BinderMetricsAutoConfiguration.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.config.metrics;
+
+import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.binder.Binder;
+import org.springframework.cloud.stream.metrics.BinderMetricsEmitter;
+import org.springframework.cloud.stream.metrics.BootMetricJsonSerializer;
+import org.springframework.cloud.stream.metrics.Emitter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Vinicius Carvalho
+ */
+@Configuration
+@ConditionalOnClass({Binder.class})
+@EnableScheduling
+@EnableBinding(Emitter.class)
+@EnableConfigurationProperties(StreamMetricsProperties.class)
+@ConditionalOnProperty("spring.cloud.stream.bindings."+ Emitter.METRICS_CHANNEL_NAME + ".destination")
+public class BinderMetricsAutoConfiguration {
+
+	@Bean
+	public BinderMetricsEmitter binderMetricsExporter(MetricsEndpoint endpoint) {
+		return new BinderMetricsEmitter(endpoint);
+	}
+
+	@Bean
+	public BootMetricJsonSerializer metricJsonSerializer() {
+		return new BootMetricJsonSerializer();
+	}
+
+}
---
+++ spring-cloud-stream-metrics/src/main/java/org/springframework/cloud/stream/config/metrics/StreamMetricsProperties.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.config.metrics;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
+
+/**
+ * @author Vinicius Carvalho
+ */
+@ConfigurationProperties(prefix = "spring.cloud.stream.metrics")
+public class StreamMetricsProperties {
+
+
+	private String prefix;
+
+	@Value("${spring.application.name:application}")
+	private String applicationName;
+
+	private String metricName;
+
+	private String[] includes;
+
+	private String[] excludes;
+
+	private String[] properties;
+
+	private Long delayMillis;
+
+	public String[] getIncludes() {
+		return includes;
+	}
+
+	public void setIncludes(String[] includes) {
+		this.includes = includes;
+	}
+
+	public String[] getExcludes() {
+		return excludes;
+	}
+
+	public void setExcludes(String[] excludes) {
+		this.excludes = excludes;
+	}
+
+	public Long getDelayMillis() {
+		return delayMillis;
+	}
+
+	public void setDelayMillis(Long delayMillis) {
+		this.delayMillis = delayMillis;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+	public String[] getProperties() {
+		return properties;
+	}
+
+	public void setProperties(String[] properties) {
+		this.properties = properties;
+	}
+
+	public String getMetricName() {
+		if(this.metricName == null){
+			this.metricName = resolveMetricName();
+		}
+		return metricName;
+	}
+
+	private String resolveMetricName(){
+		StringBuffer name = new StringBuffer(this.applicationName);
+		if(!StringUtils.isEmpty(this.prefix)){
+			String prefix = this.prefix;
+			if(prefix.lastIndexOf(".") == -1){
+				prefix += ".";
+			}
+			name.insert(0,prefix);
+		}
+		return name.toString();
+	}
+
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+}
