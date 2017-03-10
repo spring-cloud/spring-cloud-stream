795a70b2ff78ef3a51fd7313ae03a91d2fd0cc1c
Gary Russell <grussell@pivotal.io>
2017-03-10 08:55:51 -0500

    kGH-21: Add BinderHeaders.PARTITION_OVERRIDE
    See spring-cloud/spring-cloud-stream-binder-kafka#109
    See #851

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/BinderHeaders.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/BinderHeaders.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016 the original author or authors.
+ * Copyright 2015-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +38,12 @@ public final class BinderHeaders {
 	 * observe this value when sending data on the transport.
 	 */
 	public static final String PARTITION_HEADER = "partition";
+
+	/**
+	 * Indicates the target partition of an outbound message. Overrides
+	 * any partition selected by the binder.
+	 */
+	public static final String PARTITION_OVERRIDE = "partitionOverride";
 
 	/**
 	 * The headers that will be propagated, by default, by binder implementations
