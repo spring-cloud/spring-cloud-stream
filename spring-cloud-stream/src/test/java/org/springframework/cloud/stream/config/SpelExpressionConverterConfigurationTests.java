d978475e836744ef62edeab0d5d0c3e5f76cd110
Artem Bilan <abilan@pivotal.io>
2018-01-11 20:04:46 -0500

    Replace propAccessorInjector with the SpelPAReg
    After changing the `propertyAccessorInjector` `BeanPostProcessor`
    just to a simple `InitializingBean` there is no guarantee which bean
    will be accessed from the autowire phase.
    There are some cases like `@PostConstruct` on the `@Configuration`
    wich may be called before this new `propertyAccessorInjector` bean
    and we end up with `EvaluationContext` not supplied with additional
    `PropertyAccessors`
    
    * For proper `IntegrationEvaluationContextFactoryBean` initialization
    add `SpelPropertyAccessorRegistrar` for custom `PA` configuration
    * Remove `propertyAccessorInjector` bean altogether
    * Resolve deprecation in the `BindingServiceConfiguration`
    * Modify `SpelExpressionConverterConfigurationTests` to confirm that
    even `@PostConstruct` on `@Configuration` works as expected

--- spring-cloud-stream/src/test/java/org/springframework/cloud/stream/config/SpelExpressionConverterConfigurationTests.java
+++ spring-cloud-stream/src/test/java/org/springframework/cloud/stream/config/SpelExpressionConverterConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,16 @@
 
 package org.springframework.cloud.stream.config;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -26,9 +33,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.utils.MockBinderRegistryConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
+import org.springframework.expression.PropertyAccessor;
+import org.springframework.integration.expression.ExpressionUtils;
+import org.springframework.integration.json.JsonPropertyAccessor;
+import org.springframework.integration.test.util.TestUtils;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,6 +50,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Tests for SpelExpressionConverterConfiguration.
  *
  * @author Eric Bottard
+ * @author Artem Bilan
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = SpelExpressionConverterConfigurationTests.Config.class, properties = "expression: a.b")
@@ -45,9 +59,28 @@ public class SpelExpressionConverterConfigurationTests {
 	@Autowired
 	private Pojo pojo;
 
+	@Autowired
+	private EvaluationContext evaluationContext;
+
+	@Autowired
+	private Config config;
+
+
 	@Test
+	@SuppressWarnings("unchecked")
 	public void converterCorrectlyInstalled() {
-		assertThat(pojo.getExpression().getValue("{\"a\": {\"b\": 5}}").toString()).isEqualTo("5");
+		Expression expression = pojo.getExpression();
+		assertThat(expression.getValue("{\"a\": {\"b\": 5}}").toString()).isEqualTo("5");
+
+		List<PropertyAccessor> propertyAccessors =
+				TestUtils.getPropertyValue(this.evaluationContext, "propertyAccessors", List.class);
+
+		assertThat(propertyAccessors).hasAtLeastOneElementOfType(JsonPropertyAccessor.class);
+
+		propertyAccessors =
+				TestUtils.getPropertyValue(this.config.evaluationContext, "propertyAccessors", List.class);
+
+		assertThat(propertyAccessors).hasAtLeastOneElementOfType(JsonPropertyAccessor.class);
 	}
 
 	@ConfigurationProperties
@@ -69,7 +102,26 @@ public class SpelExpressionConverterConfigurationTests {
 	@EnableAutoConfiguration
 	@Import(MockBinderRegistryConfiguration.class)
 	@EnableConfigurationProperties(Pojo.class)
-	public static class Config {
+	public static class Config implements BeanFactoryAware {
+
+		private BeanFactory beanFactory;
+
+		private EvaluationContext evaluationContext;
+
+		@Override
+		public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+			this.beanFactory = beanFactory;
+		}
+
+		@Bean
+		public EvaluationContext evaluationContext() {
+			return ExpressionUtils.createStandardEvaluationContext(this.beanFactory);
+		}
+
+		@PostConstruct
+		public void setup() {
+			this.evaluationContext = ExpressionUtils.createStandardEvaluationContext(this.beanFactory);
+		}
 
 	}
 
