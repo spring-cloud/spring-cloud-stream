dff7416a7ded0d37152ca11f741d727feb8ca742
Oleg Zhurakousky <ozhurakousky@pivotal.io>
2018-02-27 13:12:26 -0500

    GH-1055 Added support for Pausable
    Added support for Pausable lifecycle to Binding and its implementation
    Added additional actuator endpoints
    
    Addressed PR comments

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/DefaultBinding.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/DefaultBinding.java
@@ -20,6 +20,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.springframework.context.Lifecycle;
+import org.springframework.integration.endpoint.Pausable;
 import org.springframework.integration.support.context.NamedComponent;
 import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
@@ -93,6 +94,26 @@ public class DefaultBinding<T> implements Binding<T> {
 	public final synchronized void stop() {
 		if (this.isRunning()) {
 			this.lifecycle.stop();
+		}
+	}
+	
+	@Override
+	public final synchronized void pause() {
+		if (this.lifecycle instanceof Pausable) {
+			( (Pausable) this.lifecycle).pause();
+		}
+		else {
+			logger.warn("Attempted to pause a component that does not support Pausable " + this.lifecycle);
+		}
+	}
+	
+	@Override
+	public final synchronized void resume() {
+		if (this.lifecycle instanceof Pausable) {
+			( (Pausable) this.lifecycle).resume();
+		}
+		else {
+			logger.warn("Attempted to resume a component that does not support Pausable " + this.lifecycle);
 		}
 	}
 
