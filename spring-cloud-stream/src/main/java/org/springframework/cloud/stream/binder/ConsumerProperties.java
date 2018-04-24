61bce5272270b8f4e6b6ff8bed8c4be85450ae1d
Soby Chacko <schacko@pivotal.io>
2018-04-24 15:25:14 -0400

    Native multiple input binding changes
    If the binder implementation enables native multiple input binding, skip individual
    consumer binding per binding target (for example - from a comma separated targets
    provided as input destination property).
    
    By default, native multiple input binding is disabled at the core framework level
    and the binder implementation explicitly needs to override that to enable it.
    
    Fixes #1365
    
    Chaning the boolean flag for multiple input binding to multiplex

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/ConsumerProperties.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/ConsumerProperties.java
@@ -123,6 +123,18 @@ public class ConsumerProperties {
 	 */
 	private boolean useNativeDecoding;
 
+	/**
+	 * When set to true, the underlying binder will natively multiplex destinations on the same input binding.
+	 * For example, in the case of a comma separated multiple destinations, the core framework will skip binding
+	 * them individually if this is set to true, but delegate that responsibility to the binder.
+	 *
+	 * By default this property is set to `false` and the binder will individually bind each destinations in case
+	 * of a comma separated multi destination list. The individual binder implementations that need to support multiple
+	 * input bindings natively (multiplex) can enable this property. Under normal circumstances, the end users are
+	 * not expected to enable or disable this property directly.
+	 */
+	private boolean multiplex;
+
 	@Min(value = 1, message = "Concurrency should be greater than zero.")
 	public int getConcurrency() {
 		return concurrency;
@@ -208,5 +220,13 @@ public class ConsumerProperties {
 
 	public void setUseNativeDecoding(boolean useNativeDecoding) {
 		this.useNativeDecoding = useNativeDecoding;
+	}
+
+	public boolean isMultiplex() {
+		return multiplex;
+	}
+
+	public void setMultiplex(boolean multiplex) {
+		this.multiplex = multiplex;
 	}
 }
