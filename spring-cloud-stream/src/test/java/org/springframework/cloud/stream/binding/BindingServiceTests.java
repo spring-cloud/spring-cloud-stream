55458034b37ff1c1491deb2649744859d4f0fd2b
Oleg Zhurakousky <ozhurakousky@pivotal.io>
2018-06-25 23:24:09 -0400

    GH-1234 Fixed broken test
    Resolves #1234

--- spring-cloud-stream/src/test/java/org/springframework/cloud/stream/binding/BindingServiceTests.java
+++ spring-cloud-stream/src/test/java/org/springframework/cloud/stream/binding/BindingServiceTests.java
@@ -27,7 +27,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
@@ -396,7 +395,6 @@ public class BindingServiceTests {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Test
-	@Ignore // Inconsistent, needs fixing
 	public void testLateBindingConsumer() throws Exception {
 		BindingServiceProperties properties = new BindingServiceProperties();
 		properties.setBindingRetryInterval(1);
@@ -428,7 +426,7 @@ public class BindingServiceTests {
 				Binding.class);
 		int n = 0;
 		while (n++ < 300 && delegate == null) {
-			Thread.sleep(100);
+			Thread.sleep(200);
 		}
 		assertThat(delegate).isSameAs(mockBinding);
 		service.unbindConsumers(inputChannelName);
