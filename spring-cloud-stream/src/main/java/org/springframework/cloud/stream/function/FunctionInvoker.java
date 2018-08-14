0f1bc1c16541679c12a47de5f0eef25f21d9725e
Oleg Zhurakousky <ozhurakousky@pivotal.io>
2018-08-14 15:24:14 +0200

    GH-1437 Added initial set of components to support SCF
    Added initial set of supporting classes, tests and configuration to support integration of Spring Cloud Function with the current set of streaming binders.
    This commit primarily addresses the Source aspect of such integration

---
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/function/FunctionInvoker.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.function;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.catalog.FunctionInspector;
+import org.springframework.cloud.stream.converter.CompositeMessageConverterFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.converter.CompositeMessageConverter;
+import org.springframework.util.Assert;
+
+/**
+ *
+ * @author Oleg Zhurakousky
+ *
+ * @param <I> the payload type of the input Message
+ * @param <O> the payload type of the output Message
+ *
+ * @since 2.1
+ */
+class FunctionInvoker<I, O> implements Function<Flux<Message<I>>, Flux<Message<O>>>  {
+
+	private final Log logger = LogFactory.getLog(this.getClass());
+
+	private final Class<?> inputClass;
+
+	private final Function<Flux<?>, Flux<?>> userFunction;
+
+	private final CompositeMessageConverter messageConverter;
+
+	FunctionInvoker(String functionName, FunctionCatalog functionCatalog, FunctionInspector functionInspector,
+						CompositeMessageConverterFactory compositeMessageConverterFactory) {
+		this.userFunction = functionCatalog.lookup(Function.class, functionName);
+		Assert.notNull(this.userFunction, "userFunction: " + functionName + " can not be located.");
+		this.messageConverter = compositeMessageConverterFactory.getMessageConverterForAllRegistered();
+		FunctionType functionType = functionInspector.getRegistration(this.userFunction).getType();
+		this.inputClass = functionType.getInputType();
+	}
+
+	@Override
+	public Flux<Message<O>> apply(Flux<Message<I>> input) {
+		AtomicReference<Message<I>> originalMessage = new AtomicReference<Message<I>>();
+		return input
+			.doOnNext(originalMessage::set) 		// to preserve the original message
+			.map(this::resolveArgument)				// resolves argument type before invocation of user function
+			.transform(f -> userFunction.apply(f))	// invoke user function
+			.map(resultMessage -> toMessage(resultMessage, originalMessage.get())); // create output message
+
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> Message<O> toMessage(T value, Message<I> originalMessage) {
+		logger.debug("Converting result back to message using the original message: " + originalMessage);
+		if (value instanceof Message) {
+			value = (T) ((Message<?>)value).getPayload();
+		}
+		Message<O> result = (Message<O>) this.messageConverter.toMessage(value, originalMessage.getHeaders());
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> T resolveArgument(Message<I> message) {
+		logger.debug("Resolving input argument from message: " + message);
+		T result = this.shouldConvertFromMessage(message)
+				? (T) this.messageConverter.fromMessage(message, this.inputClass)
+						: (T) message;
+		if (result == message && !this.inputClass.isAssignableFrom(message.getClass())) {
+			result = (T) message.getPayload();
+		}
+		return result;
+	}
+
+	private boolean shouldConvertFromMessage(Message<?> message) {
+		return !this.inputClass.isAssignableFrom(byte[].class) &&
+				!this.inputClass.isAssignableFrom(Object.class);
+	}
+}
---
+++ spring-cloud-stream/src/test/java/org/springframework/cloud/stream/function/FunctionSupportTests.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.function;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlowBuilder;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.scheduling.support.PeriodicTrigger;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.util.StringUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ * @author Oleg Zhurakousky
+ *
+ * @since 2.1
+ *
+ */
+public class FunctionSupportTests {
+
+	@Test
+	public void testFunctionIsAppliedToMessageSource() {
+		ApplicationContext context = new SpringApplicationBuilder(TestChannelBinderConfiguration.getCompleteConfiguration(MyFunctionsConfiguration.class))
+				.web(WebApplicationType.NONE).run("--spring.cloud.stream.function.name=toUpperCase",  "--spring.jmx.enabled=false");
+
+		OutputDestination target = context.getBean(OutputDestination.class);
+		assertThat(target.receive(1000).getPayload()).isEqualTo("HELLO FUNCTION".getBytes(StandardCharsets.UTF_8));
+	}
+
+	@Test
+	public void testComposedFunctionIsAppliedToMessageSource() {
+		ApplicationContext context = new SpringApplicationBuilder(TestChannelBinderConfiguration.getCompleteConfiguration(MyFunctionsConfiguration.class))
+				.web(WebApplicationType.NONE).run("--spring.cloud.stream.function.name=toUpperCase|concatWithSelf", "--spring.jmx.enabled=false");
+
+		OutputDestination target = context.getBean(OutputDestination.class);
+		assertThat(target.receive(1000).getPayload()).isEqualTo("HELLO FUNCTION:HELLO FUNCTION".getBytes(StandardCharsets.UTF_8));
+	}
+
+	@SpringBootApplication
+	@Import(SourceConfiguration.class)
+	public static class MyFunctionsConfiguration {
+		@Bean
+		public Function<String, String> toUpperCase() {
+			return x -> x.toUpperCase();
+		}
+
+		@Bean
+		public Function<String, String> concatWithSelf() {
+			return x -> x + ":" + x;
+		}
+	}
+
+	@EnableBinding(Source.class)
+	public static class SourceConfiguration {
+
+		@Autowired
+		private Source source;
+
+		@Autowired
+		private FunctionProperties functionProperties;
+
+		@Bean
+		public IntegrationFlow messageSourceFlow(FunctionSupport<?, ?> functionSupport) {
+			Supplier<Message<String>> messageSource = () -> MessageBuilder.withPayload("hello function")
+					.setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.TEXT_PLAIN).build();
+
+			PollerMetadata pm = new PollerMetadata();
+			pm.setTrigger(new PeriodicTrigger(1, TimeUnit.SECONDS));
+			pm.setMaxMessagesPerPoll(1);
+			IntegrationFlowBuilder flowBuilder = IntegrationFlows.from(messageSource);
+
+			if (StringUtils.hasText(functionProperties.getName())) {
+				functionSupport.applyFunctionToIntegrationFlow(functionProperties.getName(), flowBuilder, message -> source.output().send(message));
+			}
+			else {
+				flowBuilder = flowBuilder.channel(source.output());
+			}
+
+			return flowBuilder.get();
+		}
+	}
+}
