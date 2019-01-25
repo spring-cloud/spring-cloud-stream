e15da50939d0bf751ab5d3fb04171a4ce96e7bad
David Turanski <dturanski@pivotal.io>
2019-01-25 09:51:08 -0500

    Add receive condition

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/PollableMessageSourcePoller.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binder/PollableMessageSourcePoller.java
@@ -16,6 +16,8 @@
 
 package org.springframework.cloud.stream.binder;
 
+import java.util.function.BooleanSupplier;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.integration.endpoint.AbstractPollingEndpoint;
 import org.springframework.integration.scheduling.PollerMetadata;
@@ -39,24 +41,34 @@ public class PollableMessageSourcePoller extends AbstractPollingEndpoint {
 
 	private final ParameterizedTypeReference<?> parameterizedTypeReference;
 
-	private TaskScheduler taskScheduler = new ConcurrentTaskScheduler();
+	private  final BooleanSupplier receiveCondition;
+
+	public static Builder builder() {
+		return new Builder();
+	}
 
 	/**
-	 * @param pollerMetadata the {@link PollerMetadata} to configure the poller.
-	 * @param messageSource the {@link PollableMessageSource}.
-	 * @param messageHandler the {@link MessageHandler} to apply on received messages.
+	 * @param pollerMetadata             the {@link PollerMetadata} to configure the poller.
+	 * @param messageSource              the {@link PollableMessageSource}.
+	 * @param messageHandler             the {@link MessageHandler} to apply on received messages.
 	 * @param parameterizedTypeReference the {@link ParameterizedTypeReference} indicating the target payload type.
+	 * @param taskScheduler              the {@link TaskScheduler} to use.
+	 * @param receiveCondition           the receive condition.
 	 */
-	public PollableMessageSourcePoller(PollerMetadata pollerMetadata, PollableMessageSource messageSource,
-		MessageHandler messageHandler, @Nullable ParameterizedTypeReference<?> parameterizedTypeReference) {
+	private PollableMessageSourcePoller(PollerMetadata pollerMetadata, PollableMessageSource messageSource,
+		MessageHandler messageHandler, @Nullable ParameterizedTypeReference<?> parameterizedTypeReference,
+		TaskScheduler taskScheduler, BooleanSupplier receiveCondition) {
 
 		Assert.notNull(pollerMetadata, "'pollerMetadata' cannot be null");
 		Assert.notNull(messageSource, "'messageSource' cannot be null");
 		Assert.notNull(messageHandler, "'messageHandler' cannot be null");
+		Assert.notNull(taskScheduler, "'taskScheduler' cannot be null");
+		Assert.notNull(receiveCondition, "'receiveCondition cannot be null");
 
 		this.messageHandler = messageHandler;
 		this.messageSource = messageSource;
 		this.parameterizedTypeReference = parameterizedTypeReference;
+		this.receiveCondition = receiveCondition;
 		this.setTrigger(pollerMetadata.getTrigger());
 		this.setMaxMessagesPerPoll(pollerMetadata.getMaxMessagesPerPoll());
 		this.setAdviceChain(pollerMetadata.getAdviceChain());
@@ -66,22 +78,110 @@ public class PollableMessageSourcePoller extends AbstractPollingEndpoint {
 		this.setTaskScheduler(taskScheduler);
 	}
 
-	/**
-	 *  @param pollerMetadata the {@link PollerMetadata} to configure the poller.
- 	 * @param messageSource the {@link PollableMessageSource}.
- 	 * @param messageHandler the {@link MessageHandler} to apply on received messages.
- 	 */
-	public PollableMessageSourcePoller(PollerMetadata pollerMetadata, PollableMessageSource messageSource,
-		MessageHandler messageHandler) {
-		this(pollerMetadata, messageSource, messageHandler, null);
+	@Override
+	protected Message<?> receiveMessage() {
+		return receiveCondition.getAsBoolean() ?
+			this.messageSource.receive(this.parameterizedTypeReference) :
+			null;
 	}
 
 	@Override
-	protected Message<?> receiveMessage() {
-		return this.messageSource.receive(this.parameterizedTypeReference);
-	}
-	@Override
 	protected void handleMessage(Message<?> message) {
 		messageHandler.handleMessage(message);
+	}
+
+	public static class Builder {
+
+		private MessageHandler messageHandler;
+
+		private PollableMessageSource messageSource;
+
+		private ParameterizedTypeReference<?> parameterizedTypeReference;
+
+		private TaskScheduler taskScheduler = new ConcurrentTaskScheduler();
+
+		private BooleanSupplier receiveCondition = () -> true;
+
+		private PollerMetadata pollerMetadata;
+
+		private Builder() {
+		}
+
+		/**
+		 * Set the {@link MessageHandler} to invoke on received messages.
+		 *
+		 * @param messageHandler the message handler.
+		 * @return the Builder instance.
+		 */
+		public Builder messageHandler(MessageHandler messageHandler) {
+			this.messageHandler = messageHandler;
+			return this;
+		}
+
+		/**
+		 * Set the {@link PollableMessageSource}>
+		 *
+		 * @param messageSource the message source.
+		 * @return the Builder instance.
+		 */
+		public Builder messageSource(PollableMessageSource messageSource) {
+			this.messageSource = messageSource;
+			return this;
+		}
+
+		/**
+		 * Set a {@link PollerMetadata} to configure the poller.
+		 *
+		 * @param pollerMetadata
+		 * @return the Builder instance.
+		 */
+		public Builder pollerMetadata(PollerMetadata pollerMetadata) {
+			this.pollerMetadata = pollerMetadata;
+			return this;
+		}
+
+		/**
+		 * Set a {@link BooleanSupplier} as a condition for receiving messages.
+		 *
+		 * @param receiveCondition the receive condition.
+		 * @return the Builder instance.
+		 */
+		public Builder receiveCondition(BooleanSupplier receiveCondition) {
+			this.receiveCondition = receiveCondition;
+			return this;
+		}
+
+		/**
+		 * Set a {@link ParameterizedTypeReference} indicating the target payload type.
+		 *
+		 * @param parameterizedTypeReference the type.
+		 * @return the Builder instance.
+		 */
+		public Builder parameterizedTypeReference(ParameterizedTypeReference<?> parameterizedTypeReference) {
+			this.parameterizedTypeReference = parameterizedTypeReference;
+			return this;
+		}
+
+		/**
+		 * Set a {@link TaskScheduler} the default is {@link ConcurrentTaskScheduler}.
+		 *
+		 * @param taskScheduler the taskScheduler.
+		 * @return the Builder instance.
+		 */
+		public Builder taskScheduler(TaskScheduler taskScheduler) {
+			this.taskScheduler = taskScheduler;
+			return this;
+		}
+
+		public PollableMessageSourcePoller build() {
+			return new PollableMessageSourcePoller(
+				this.pollerMetadata,
+				this.messageSource,
+				this.messageHandler,
+				this.parameterizedTypeReference,
+				this.taskScheduler,
+				this.receiveCondition);
+		}
+
 	}
 }
--- spring-cloud-stream/src/test/java/org/springframework/cloud/stream/binder/PollableMessageSourcePollerTests.java
+++ spring-cloud-stream/src/test/java/org/springframework/cloud/stream/binder/PollableMessageSourcePollerTests.java
@@ -16,21 +16,31 @@
 
 package org.springframework.cloud.stream.binder;
 
+import java.util.Collections;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BooleanSupplier;
 
 import org.junit.Test;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.binder.test.TestChannelBinder;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.lang.Nullable;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.scheduling.support.PeriodicTrigger;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,10 +51,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PollableMessageSourcePollerTests {
 
 	@Test
-	public void configuredPoller() throws InterruptedException {
+	public void unconditionalPoller() throws InterruptedException {
 		ApplicationContext context =
 			new SpringApplicationBuilder(TestChannelBinderConfiguration.getCompleteConfiguration(TestApplication.class))
 				.web(WebApplicationType.NONE)
+				.run();
+		CountDownLatch countDownLatch = context.getBean(CountDownLatch.class);
+		countDownLatch.await(1, TimeUnit.SECONDS);
+		assertThat(countDownLatch.getCount()).isZero();
+
+	}
+
+	@Test
+	public void pollerWithReceiveCondition() throws InterruptedException {
+		ApplicationContext context =
+			new SpringApplicationBuilder(TestChannelBinderConfiguration.getCompleteConfiguration(TestApplication.class))
+				.web(WebApplicationType.NONE)
+				.profiles("conditional")
 				.run();
 		CountDownLatch countDownLatch = context.getBean(CountDownLatch.class);
 		countDownLatch.await(1, TimeUnit.SECONDS);
@@ -69,19 +92,69 @@ public class PollableMessageSourcePollerTests {
 		}
 
 		@Bean
-		MessageHandler messageHandler(CountDownLatch countDownLatch) {
-			return m -> {
-				assertThat(m.getPayload()).isEqualTo("polled data");
-				countDownLatch.countDown();
+		public BeanPostProcessor messageSourceDelegatePostProcessor() {
+			return new BeanPostProcessor() {
+				final AtomicInteger count = new AtomicInteger();
+				@Override
+				public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+					if (bean instanceof TestChannelBinder) {
+						((TestChannelBinder) bean).setMessageSourceDelegate(() ->
+							new GenericMessage<>(("polled data-" + count.getAndIncrement()).getBytes(),
+								Collections.singletonMap("contentType", "text/plain"))
+						);
+					}
+					return bean;
+				}
 			};
 		}
 
 		@Bean
+		@Profile("!conditional")
+		MessageHandler messageHandler(CountDownLatch countDownLatch) {
+			return m -> {
+				assertThat((String) m.getPayload()).startsWith("polled data");
+				countDownLatch.countDown();
+			};
+		}
+
+		@Profile("conditional")
+		@Configuration
+		static class ConditionalConsumerConfig {
+
+			@Bean
+			BooleanSupplier conditionalConsumer(CountDownLatch countDownLatch) {
+				return () -> {
+					boolean ready = countDownLatch.getCount() % 2 == 1;
+					countDownLatch.countDown();
+					return ready;
+				};
+			}
+
+			@Bean
+			MessageHandler messageHandler() {
+				return m -> {
+					System.out.println("! received message " + m);
+					assertThat((String) m.getPayload()).matches("polled data-[0,1]");
+				};
+			}
+		}
+
+		@Bean
 		public PollableMessageSourcePoller pollingEndpoint(PollableMessageSource source,
-			PollerMetadata pollerMetadata, MessageHandler messageHandler) {
-			return new PollableMessageSourcePoller(pollerMetadata,
-				source, messageHandler, new ParameterizedTypeReference<String>() {
-			});
+			PollerMetadata pollerMetadata, MessageHandler messageHandler, @Nullable BooleanSupplier receiveCondition) {
+			PollableMessageSourcePoller.Builder builder = PollableMessageSourcePoller.builder()
+				.messageSource(source)
+				.messageHandler(messageHandler)
+				.pollerMetadata(pollerMetadata)
+				.parameterizedTypeReference(new ParameterizedTypeReference<String>() {
+				});
+
+			if (receiveCondition != null) {
+				builder.receiveCondition(receiveCondition);
+
+			}
+
+			return builder.build();
 		}
 
 	}
@@ -92,4 +165,5 @@ public class PollableMessageSourcePollerTests {
 		PollableMessageSource source();
 
 	}
+
 }
