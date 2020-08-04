b47f02f16e51689ed24013dbe2407d27e4c5670e
Oleg Zhurakousky <ozhurakousky@pivotal.io>
2020-08-04 16:40:27 +0200

    GH-2011 Remove log override from test binder
    Resolves #2011

--- spring-cloud-stream-binder-test/src/main/java/org/springframework/cloud/stream/binder/BinderTestEnvironmentPostProcessor.java
+++ spring-cloud-stream-binder-test/src/main/java/org/springframework/cloud/stream/binder/BinderTestEnvironmentPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 the original author or authors.
+ * Copyright 2016-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,6 @@ public class BinderTestEnvironmentPostProcessor implements EnvironmentPostProces
 		Map<String, Object> propertiesToAdd = new HashMap<>();
 		propertiesToAdd.put("logging.pattern.console",
 				"%d{ISO8601} %5p %t %c{2}:%L - %m%n");
-		propertiesToAdd.put("logging.level.root", "WARN");
 		environment.getPropertySources().addLast(
 				new MapPropertySource("binderTestPropertiesConfig", propertiesToAdd));
 	}
