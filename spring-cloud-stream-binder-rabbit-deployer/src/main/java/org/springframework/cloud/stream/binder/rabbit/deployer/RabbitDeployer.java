e60db263847aeff3076d1be9617f0476e383cf30
Oleg Zhurakousky <ozhurakousky@vmware.com>
2021-08-02 17:10:40 +0200

    GH-331 Initial support for binder deployer
    Resolves #331

--- spring-cloud-stream-binder-rabbit-deployer/src/main/java/org/springframework/cloud/stream/binder/rabbit/deployer/RabbitDeployer.java
+++ spring-cloud-stream-binder-rabbit-deployer/src/main/java/org/springframework/cloud/stream/binder/rabbit/deployer/RabbitDeployer.java
@@ -16,11 +16,19 @@
 
 package org.springframework.cloud.stream.binder.rabbit.deployer;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.function.context.FunctionProperties;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.support.MessageBuilder;
 
 
 /**
@@ -32,24 +40,42 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class RabbitDeployer {
 
+	/*
+	 * SpringApplication.run(RabbitDeployer.class,
+				"--spring.cloud.function.definition=reverseFunction",
+				"--spring.cloud.function.location=/bootjar-1.0.0.RELEASE-exec.jar",
+				"--spring.cloud.function.function-class=function.example.ReverseFunction"
+				);
+	 */
 	public static void main(String[] args) {
-		SpringApplication.run(RabbitDeployer.class,
-				//target/it/bootjar/target/bootjar-1.0.0.RELEASE-exec.jar
-				"--spring.cloud.function.definition=reverseFunction;echo",
-				"--spring.cloud.function.location=/Users/ozhurakousky/dev/repo/spring-cloud-function/spring-cloud-function-deployer/target/it/bootjar/target/bootjar-1.0.0.RELEASE-exec.jar",
-				"--spring.cloud.function.function-class=function.example.ReverseFunction");
+		SpringApplication.run(RabbitDeployer.class);
 	}
 
-
 	@Bean
-	public Function<String, String> echo() {
-		// java -jar timesource-kafka.jar
+	@ConditionalOnProperty(name = FunctionProperties.PREFIX + ".rsocket.enabled", matchIfMissing = true)
+	public Function<Message<byte[]>, Message<byte[]>> gateway(StreamBridge bridge) {
+		return message -> {
+			String destinationName = (String) message.getHeaders().get("target_destination");
+			bridge.send(destinationName, message);
+			return MessageBuilder.withPayload("Successfully sent to reverseFunction-in-0".getBytes()).build();
+		};
+	}
 
-		// java -jar rabbit-bundle.jar -Dspring.cloud.function.location=.....jar
-		// java -jar rabbit-kafka-bundle.jar -Dspring.cloud.function.location=.....jar
-
-		// java -jar rabbit-rsocket-bundle.jar  = GATEWAY
-		return v -> v;
+	/*
+	 * Just like any other stream bean. This one will subscribe to broker destination (using regular stream mechanisms)
+	 * and using some configuration provided by the user will propagate message to remote (rsocket) subscriber
+	 */
+	@Bean
+	@ConditionalOnProperty(name = FunctionProperties.PREFIX + ".rsocket.enabled", matchIfMissing = true)
+	public Consumer<Message<byte[]>> delegatingConsumer(RSocketRequester.Builder rsocketRequesterBuilder, Environment environment) {
+		String host = environment.getProperty("spring.cloud.function.rsocket.subscriber.host");
+		String port = environment.getProperty("spring.cloud.function.rsocket.subscriber.port");
+		return message -> {
+//			rsocketRequesterBuilder.tcp("host", Integer.valueOf(port))
+//			.route("pojoToString")
+//			.data(message)
+//			.retrieveMono(String.class);
+		};
 	}
 
 	// Step-1 - rabbit-bundle.jar(time) | rabbit-bundle.jar(log) - Step One - local
@@ -58,7 +84,5 @@ public class RabbitDeployer {
 	// Step-4 - Kubernetes
 
 	//http | rabbit-rsocket-bundle.jar(producer) | python | rabbit-rsocket-bundle.jar(consumer) | rabbit-bundle.jar(log)
-
-	//http => pyjon
 
 }
