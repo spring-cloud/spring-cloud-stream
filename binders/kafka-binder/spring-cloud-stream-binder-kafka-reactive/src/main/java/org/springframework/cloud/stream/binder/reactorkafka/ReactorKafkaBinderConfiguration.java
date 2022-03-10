f3ccc7b72de7e7aa5279528b3be4a990225b1100
Gary Russell <grussell@vmware.com>
2022-03-10 13:35:29 -0500

    GH-2293: Initial Commit Reactor-Kafka Binder
    Note that it requires the message channel to be a `FluxMessageChannel`.
    
    It currently only supports `Consumer<ReceiverRecord<?, ?>>` (no conversion).
    
    The function binding should use the message channel directly instead of bridging
    the direct channel to a reactive channel.
    
    Producer Side PoC
    
    Binder specific changes in ReactorKafkaBinder
    
    Binder configuration
    
    More changes to the reactive binder
    
    Move KafkaBinderEnvironmentPostProcessor to the core module
    
    Change binder key in spring.binders
    
    Adding an e2e IT test for the reactive binder
    
    Add lifecycle to integration adapters.
    
    Remove unnecessary doStop().
    
    Fix import order.

---
+++ binders/kafka-binder/spring-cloud-stream-binder-kafka-reactive/src/main/java/org/springframework/cloud/stream/binder/reactorkafka/ReactorKafkaBinderConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.binder.reactorkafka;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.binder.Binder;
+import org.springframework.cloud.stream.binder.kafka.properties.KafkaBinderConfigurationProperties;
+import org.springframework.cloud.stream.binder.kafka.properties.KafkaExtendedBindingProperties;
+import org.springframework.cloud.stream.binder.kafka.provisioning.AdminClientConfigCustomizer;
+import org.springframework.cloud.stream.binder.kafka.provisioning.KafkaTopicProvisioner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnMissingBean(Binder.class)
+@Import({ KafkaAutoConfiguration.class })
+@EnableConfigurationProperties({ KafkaExtendedBindingProperties.class })
+public class ReactorKafkaBinderConfiguration {
+
+	@Bean
+	KafkaBinderConfigurationProperties configurationProperties(
+			KafkaProperties kafkaProperties) {
+		return new KafkaBinderConfigurationProperties(kafkaProperties);
+	}
+
+	@Bean
+	KafkaTopicProvisioner provisioningProvider(
+			KafkaBinderConfigurationProperties configurationProperties,
+			ObjectProvider<AdminClientConfigCustomizer> adminClientConfigCustomizer, KafkaProperties kafkaProperties) {
+		return new KafkaTopicProvisioner(configurationProperties,
+				kafkaProperties, adminClientConfigCustomizer.getIfUnique());
+	}
+
+	@Bean
+	ReactorKafkaBinder reactorKafkaBinder(KafkaBinderConfigurationProperties configurationProperties,
+			KafkaTopicProvisioner provisioningProvider) {
+
+		ReactorKafkaBinder reactorKafkaBinder = new ReactorKafkaBinder(configurationProperties, provisioningProvider);
+		return reactorKafkaBinder;
+	}
+
+}
---
+++ binders/kafka-binder/spring-cloud-stream-binder-kafka-reactive/src/test/java/org/springframework/cloud/stream/binder/reactorkafka/ReactorKafkaBinderIntegrationTests.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2022-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.binder.reactorkafka;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.condition.EmbeddedKafkaCondition;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Soby Chacko
+ * @author Gary Russell
+ */
+@EmbeddedKafka(topics = "uppercased-words")
+public class ReactorKafkaBinderIntegrationTests {
+
+	private static final EmbeddedKafkaBroker embeddedKafka = EmbeddedKafkaCondition.getBroker();
+
+	private static Consumer<String, String> consumer;
+
+	@BeforeAll
+	public static void setUp() {
+		Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("group", "false",
+				embeddedKafka);
+		consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		DefaultKafkaConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
+		consumer = cf.createConsumer();
+		embeddedKafka.consumeFromEmbeddedTopics(consumer, "uppercased-words");
+	}
+
+	@Test
+	void testEndtoEndReactorKafkaBinder() throws Exception {
+		SpringApplication app = new SpringApplication(ReactiveKafkaApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+
+		try (ConfigurableApplicationContext context = app.run(
+				"--server.port=0",
+				"--spring.jmx.enabled=false",
+				"--spring.cloud.stream.function.reactive.uppercase=true",
+				"--spring.cloud.stream.bindings.uppercase-in-0.destination=words",
+				"--spring.cloud.stream.bindings.uppercase-out-0.destination=uppercased-words",
+				"--spring.cloud.stream.kafka.binder.brokers=" + embeddedKafka.getBrokersAsString())) {
+
+			Map<String, Object> senderProps = KafkaTestUtils.producerProps(embeddedKafka);
+			DefaultKafkaProducerFactory<Integer, String> pf = new DefaultKafkaProducerFactory<>(senderProps);
+			try {
+				KafkaTemplate<Integer, String> template = new KafkaTemplate<>(pf, true);
+				template.setDefaultTopic("words");
+				template.sendDefault("foobar");
+
+				ConsumerRecord<String, String> cr = KafkaTestUtils.getSingleRecord(consumer, "uppercased-words");
+				assertThat(cr.value().equals("FOOBAR")).isTrue();
+			}
+			finally {
+				pf.destroy();
+			}
+		}
+	}
+
+	@EnableAutoConfiguration
+	public static class ReactiveKafkaApplication {
+
+		@Bean
+		public Function<Flux<String>, Flux<String>> uppercase() {
+			return s -> s.map(String::toUpperCase);
+		}
+
+	}
+}
