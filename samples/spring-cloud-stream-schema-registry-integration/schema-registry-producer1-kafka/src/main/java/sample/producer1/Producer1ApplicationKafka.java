449e6f83d85d60744f9c1ab490418c4ef8db2fbf
Chris Bono <chris.bono@gmail.com>
2022-08-29 19:30:14 -0500

    Rename SCSt schema registry sample (#2500)

---
+++ samples/spring-cloud-stream-schema-registry-integration/schema-registry-producer1-kafka/src/main/java/sample/producer1/Producer1ApplicationKafka.java
@@ -0,0 +1,45 @@
+package sample.producer1;
+
+import java.util.Random;
+import java.util.UUID;
+
+import com.example.Sensor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.cloud.stream.schema.registry.client.EnableSchemaRegistryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(proxyBeanMethods = false)
+@EnableSchemaRegistryClient
+@RestController
+public class Producer1ApplicationKafka {
+
+	private Random random = new Random();
+
+	@Autowired
+	private StreamBridge streamBridge;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Producer1ApplicationKafka.class, args);
+	}
+
+	@RequestMapping(value = "/randomMessage", method = RequestMethod.POST)
+	public String sendRandomMessage() {
+		streamBridge.send("supplier-out-0", randomSensor());
+		return "ok, have fun with v1 payload!";
+	}
+
+	private Sensor randomSensor() {
+		Sensor sensor = new Sensor();
+		sensor.setId(UUID.randomUUID() + "-v1");
+		sensor.setAcceleration(random.nextFloat() * 10);
+		sensor.setVelocity(random.nextFloat() * 100);
+		sensor.setTemperature(random.nextFloat() * 50);
+		return sensor;
+	}
+}
