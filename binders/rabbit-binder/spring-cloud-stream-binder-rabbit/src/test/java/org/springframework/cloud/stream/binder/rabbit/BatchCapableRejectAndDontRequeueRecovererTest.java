c9778d88c9c90975c58026bba28e46a1d603a3b9
Oleg Zhurakousky <ozhurakousky@vmware.com>
2022-11-29 11:30:18 -0800

    Fixing checkstyle from previous commit
    Resolves #2567

--- binders/rabbit-binder/spring-cloud-stream-binder-rabbit/src/test/java/org/springframework/cloud/stream/binder/rabbit/BatchCapableRejectAndDontRequeueRecovererTest.java
+++ binders/rabbit-binder/spring-cloud-stream-binder-rabbit/src/test/java/org/springframework/cloud/stream/binder/rabbit/BatchCapableRejectAndDontRequeueRecovererTest.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2022-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.stream.binder.rabbit;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
@@ -9,9 +26,7 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.retry.MessageBatchRecoverer;
 import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BatchCapableRejectAndDontRequeueRecovererTest {
 
@@ -26,14 +41,13 @@ class BatchCapableRejectAndDontRequeueRecovererTest {
 
 		Throwable expectedThrowable = new RuntimeException("test");
 
-		ListenerExecutionFailedException exception = assertThrows(ListenerExecutionFailedException.class,
+		ListenerExecutionFailedException exception = Assertions.assertThrows(ListenerExecutionFailedException.class,
 				() -> messageBatchRecoverer.recover(messages, expectedThrowable));
 
-		assertTrue(exception.getFailedMessages().contains(expectedMessage1));
-		assertTrue(exception.getFailedMessages().contains(expectedMessage2));
-		assertTrue(exception.getCause() instanceof AmqpRejectAndDontRequeueException);
-		assertEquals(expectedThrowable, exception.getCause().getCause());
-
+		assertThat(exception.getFailedMessages()).contains(expectedMessage1);
+		assertThat(exception.getFailedMessages()).contains(expectedMessage2);
+		assertThat(exception.getCause()).isInstanceOf(AmqpRejectAndDontRequeueException.class);
+		assertThat(expectedThrowable).isEqualTo(exception.getCause().getCause());
 	}
 
 	@Test
@@ -44,13 +58,11 @@ class BatchCapableRejectAndDontRequeueRecovererTest {
 
 		Throwable expectedThrowable = new RuntimeException("test");
 
-		ListenerExecutionFailedException exception = assertThrows(ListenerExecutionFailedException.class,
+		ListenerExecutionFailedException exception = Assertions.assertThrows(ListenerExecutionFailedException.class,
 				() -> messageBatchRecoverer.recover(expectedMessage, expectedThrowable));
 
-		assertEquals(expectedMessage, exception.getFailedMessage());
-		assertTrue(exception.getCause() instanceof AmqpRejectAndDontRequeueException);
-		assertEquals(expectedThrowable, exception.getCause().getCause());
-
+		assertThat(expectedMessage).isEqualTo(exception.getFailedMessage());
+		assertThat(exception.getCause()).isInstanceOf(AmqpRejectAndDontRequeueException.class);
+		assertThat(expectedThrowable).isEqualTo(exception.getCause().getCause());
 	}
-
 }
