2d2d4f2f5e8383288cb735f2aa2995f9dbed5add
jamesforward <james.forward@mettle.co.uk>
2023-05-22 20:15:23 +0100

    Multiple functions in Kafka Streams binder
     * Trim functionUnits so we can declare them in a multi-line fashion,
       as currently whitespace is still considered.
    
     * Polishing
    
    Resolves https://github.com/spring-cloud/spring-cloud-stream/issues/2742

---
+++ binders/kafka-binder/spring-cloud-stream-binder-kafka-streams/src/test/java/org/springframework/cloud/stream/binder/kafka/streams/KafkaStreamsBinderUtilsTests.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.binder.kafka.streams;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link KafkaStreamsBinderUtils} functions
+ *
+ * @author James Forward
+ * @author Soby Chacko
+ */
+class KafkaStreamsBinderUtilsTests {
+
+	@Test
+	void testDeriveFunctionUnitsWithVariedSpacings() {
+		final String definition = "firstFunction; secondFunction;   thirdFunction;fourthFunction";
+
+		final String[] functionUnits = KafkaStreamsBinderUtils.deriveFunctionUnits(definition);
+
+		assertThat(functionUnits.length).isEqualTo(4);
+		assertThat(functionUnits[0]).isEqualTo("firstFunction");
+		assertThat(functionUnits[1]).isEqualTo("secondFunction");
+		assertThat(functionUnits[2]).isEqualTo("thirdFunction");
+		assertThat(functionUnits[3]).isEqualTo("fourthFunction");
+	}
+}
