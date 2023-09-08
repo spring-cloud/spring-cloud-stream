5f266ce580ca4340cd3f1298928a9ba930a4a53d
Soby Chacko <chackos@vmware.com>
2023-09-08 18:27:24 -0400

    Kafka binder RuntimeHints
     - Adding missing runtime hints for Kafka binder properties
    
    Resolves https://github.com/spring-cloud/spring-cloud-stream/issues/2604

---
+++ binders/kafka-binder/spring-cloud-stream-binder-kafka/src/main/java/org/springframework/cloud/stream/binder/kafka/aot/KafkaBinderRuntimeHints.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.binder.kafka.aot;
+
+import java.util.stream.Stream;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.cloud.stream.binder.kafka.properties.KafkaBindingProperties;
+import org.springframework.cloud.stream.binder.kafka.properties.KafkaConsumerProperties;
+import org.springframework.cloud.stream.binder.kafka.properties.KafkaExtendedBindingProperties;
+import org.springframework.cloud.stream.binder.kafka.properties.KafkaProducerProperties;
+import org.springframework.lang.Nullable;
+
+/**
+ * {@link RuntimeHintsRegistrar} for the Kafka binder in Spring Cloud Stream.
+ *
+ * @author Soby Chacko
+ * @since 4.0.5
+ *
+ */
+public class KafkaBinderRuntimeHints implements RuntimeHintsRegistrar {
+	@Override
+	public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+		ReflectionHints reflectionHints = hints.reflection();
+		Stream.of(
+				KafkaConsumerProperties.class,
+				KafkaProducerProperties.class,
+				KafkaExtendedBindingProperties.class,
+				KafkaBindingProperties.class)
+			.forEach(type -> reflectionHints.registerType(type,
+				builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS)));
+	}
+}
