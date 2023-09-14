8fdf5637de66eb8780d64b5154ff8b7fb44bca6f
Soby Chacko <chackos@vmware.com>
2023-09-14 10:57:16 -0400

    Make AbstractBindingLifecycle public
    See this discussion for details:
    https://github.com/spring-cloud/spring-cloud-stream/pull/2793#issuecomment-1715420091

--- core/spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/AbstractBindingLifecycle.java
+++ core/spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/AbstractBindingLifecycle.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2017-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,10 +27,12 @@ import org.springframework.context.SmartLifecycle;
  * {@link Bindable}s.
  *
  * @author Oleg Zhurakousky
+ * @author Soby Chacko
+ *
  * @see InputBindingLifecycle
  * @see OutputBindingLifecycle
  */
-abstract class AbstractBindingLifecycle implements SmartLifecycle {
+public abstract class AbstractBindingLifecycle implements SmartLifecycle {
 
 	final BindingService bindingService;
 
