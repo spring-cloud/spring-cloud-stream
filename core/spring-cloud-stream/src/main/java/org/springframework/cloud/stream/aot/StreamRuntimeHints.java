248fe01b9a1834f23381bbf2bb05fbc310aa4d85
omercelikceng <omercelik.ceng@gmail.com>
2023-09-18 22:54:25 +0300

    Cloud Stream Core RuntimeHints
     - Adding missing runtime hints for Stream Binding (InputBindingLifecycle and OutputBindingLifecycle)
    
    Resolves spring-cloud#2793

---
+++ core/spring-cloud-stream/src/main/java/org/springframework/cloud/stream/aot/StreamRuntimeHints.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.aot;
+
+import java.util.stream.Stream;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.cloud.stream.binding.InputBindingLifecycle;
+import org.springframework.cloud.stream.binding.OutputBindingLifecycle;
+import org.springframework.lang.Nullable;
+
+/**
+ * {@link RuntimeHintsRegistrar} for the Core in Spring Cloud Stream.
+ *
+ * @author Omer Celik
+ *
+ */
+public class StreamRuntimeHints implements RuntimeHintsRegistrar {
+	@Override
+	public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+		ReflectionHints reflectionHints = hints.reflection();
+		Stream.of(
+				InputBindingLifecycle.class,
+				OutputBindingLifecycle.class)
+			.forEach(type -> reflectionHints.registerType(type,
+				builder -> builder.withMembers(MemberCategory.DECLARED_FIELDS)));
+	}
+}
