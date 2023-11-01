515923266e164df0dd127d4db8fd23a62291688d
Soby Chacko <chackos@vmware.com>
2023-11-01 15:38:04 -0400

    [Test Cleanup - Package Private]
    - spring-cloud-stream-binder-kafka-core module tests

--- binders/kafka-binder/spring-cloud-stream-binder-kafka-core/src/test/java/org/springframework/cloud/stream/binder/kafka/provisioning/KafkaTopicProvisionerTests.java
+++ binders/kafka-binder/spring-cloud-stream-binder-kafka-core/src/test/java/org/springframework/cloud/stream/binder/kafka/provisioning/KafkaTopicProvisionerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2022 the original author or authors.
+ * Copyright 2018-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ import static org.assertj.core.api.Assertions.fail;
  * @since 2.0
  *
  */
-public class KafkaTopicProvisionerTests {
+class KafkaTopicProvisionerTests {
 
 	AdminClientConfigCustomizer adminClientConfigCustomizer = adminClientProperties -> adminClientProperties.put("foo", "bar");
 
