0a18ca5ec9af6a8e0e2ff01cba1fc8dada85df66
lazare <l.giorgobiani@lider-bet.com>
2024-02-15 23:28:55 +0400

    update year and author

--- binders/kafka-binder/spring-cloud-stream-binder-kafka-streams/src/main/java/org/springframework/cloud/stream/binder/kafka/streams/KafkaStreamsMessageConversionDelegate.java
+++ binders/kafka-binder/spring-cloud-stream-binder-kafka-streams/src/main/java/org/springframework/cloud/stream/binder/kafka/streams/KafkaStreamsMessageConversionDelegate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2023 the original author or authors.
+ * Copyright 2018-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,7 @@ import org.springframework.util.StringUtils;
  *
  * @author Soby Chacko
  * @author Byungjun You
+ * @author Lazare Giorgobiani
  */
 public class KafkaStreamsMessageConversionDelegate {
 
