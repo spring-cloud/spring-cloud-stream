2a4a15bd7804df89d57d1c4145074fc1f4207e49
kutmk <noreply.kutmk@gmail.com>
2024-05-11 01:32:00 +0900

    Update copyright and author tag

--- core/spring-cloud-stream-integration-tests/src/test/java/org/springframework/cloud/stream/config/BindingHandlerAdviseTests.java
+++ core/spring-cloud-stream-integration-tests/src/test/java/org/springframework/cloud/stream/config/BindingHandlerAdviseTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2023 the original author or authors.
+ * Copyright 2018-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 /**
  * @author Oleg Zhurakousky
  * @author Soby Chacko
+ * @author Kotaro Matsumoto
  *
  */
 class BindingHandlerAdviseTests {
